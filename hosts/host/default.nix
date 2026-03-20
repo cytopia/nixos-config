7544b103b9e990335d9681156cf860a5e7f0b2a2
cytopia <cytopia@everythingcli.org>
2026-03-20 02:03:26 +0100

    restructure files

--- hosts/host/default.nix
+++ hosts/host/default.nix
@@ -4,10 +4,36 @@
 {
   programs.nix-ld.enable = true;
   imports =
-    [
-      ./hardware-configuration.nix
-      ../../modules/nixos/default.nix
-    ];
+  [
+    ./hardware-configuration.nix
+    #../../modules/nixos/default.nix
+
+    # --- Core modules ---
+    ./modules/core/network.nix
+    ./modules/core/time-locale.nix
+    ./modules/core/keyboard.nix
+    ./modules/core/users.nix
+    ./modules/core/gpu-intel.nix
+    ./modules/core/bluetooth.nix
+    ./modules/core/sound.nix
+    ./modules/core/power-management.nix
+
+    # --- CLI modules ---
+    ./modules/cli/vim.nix
+
+    # --- GUI modules ---
+    ./modules/gui/fonts.nix
+    ./modules/gui/login-manager.nix
+    ./modules/gui/display-manager.nix
+    ./modules/gui/sway.nix
+
+    # --- Programs ---
+    ./modules/programs/_default.nix
+    ./modules/programs/chromium.nix
+    ./modules/programs/thunar.nix
+    ./modules/programs/podman.nix
+    ./modules/programs/obs.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -24,6 +50,14 @@
 
   # TODO: Move this somewhere else
   programs.awsvpnclient.enable = true;
+
+  environment.systemPackages = [
+    pkgs.steam-run
+  ];
+  qt.enable = true;
+
+  # Might help chromium errors and screensharing
+  fonts.fontconfig.enable = true;
 
 
   system.stateVersion = "25.11"; # Did you read the comment?
