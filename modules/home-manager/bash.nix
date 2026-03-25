2cc213d06ee29d6bb00caa70aaed953fa38584d6
cytopia <cytopia@everythingcli.org>
2026-03-25 14:41:11 +0100

    bash/tmux

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -25,16 +25,6 @@ in
         description = "An attribute set of extra shell aliases.";
         example = { g = "git"; };
       };
-      use_bat = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Whether to alias cat to 'bat'.";
-      };
-      use_eza = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Whether to alias ls to 'eza'.";
-      };
     };
 
     # bashrc
@@ -74,6 +64,22 @@ in
       default = false;
       description = "Whether to enable direnv for bash.";
     };
+    enableEza = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to enable eza for bash.";
+    };
+    enableDircolors = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to enable dircolors for bash.";
+    };
+    enableBat = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to enable bat for bash.";
+    };
+
   };
 
 
@@ -110,15 +116,16 @@ in
         # Default args
         rgrep = "grep -r --color=auto --binary-file=without-match";
       }
-      // (if cfg.aliases.use_bat then {
+      // (if cfg.enableBat then {
         cat = "bat --plain";
+        man = "batman";
       } else {})
-      // (if cfg.aliases.use_eza then {
+      // (if cfg.enableEza then {
         # Listings: eza
-        ll = "eza -la";
-        ls = "eza --icons";
-        la = "eza -a";
-        l = "eza -l";
+        #ll = "eza -la";
+        #ls = "eza --icons";
+        #la = "eza -a";
+        #l = "eza -l";
       } else {
         # Listings: ls
         ll = "ls --color=always --group-directories-first --classify -al";
@@ -135,28 +142,44 @@ in
 
       # Do we attach Tmux for every interactive shell?
       initExtra = lib.optionalString cfg.autoAttachTmux ''
-        # Only run if:
         # 1. We are in an interactive shell ($- == *i*)
         # 2. We are NOT already inside a tmux session ($TMUX is empty)
-        # 3. We are NOT in a plain TTY (optional, ensures tmux only starts in Wayland)
+        # 3. We are NOT in a plain TTY (ensures tmux only starts in Wayland)
         if [[ $- == *i* ]] && [[ -z "$TMUX" ]] && [[ -n "$WAYLAND_DISPLAY" ]]; then
 
-          # Ensure the tmux server process knows about our current Sway environment
-          # We 'setenv' BEFORE attaching to ensure all panes get the new socket
           if command -v tmux >/dev/null; then
-            # TODO: check if we need to set the env here
-            #tmux setenv -g SWAYSOCK "$SWAYSOCK" 2>/dev/null
-            #tmux setenv -g WAYLAND_DISPLAY "$WAYLAND_DISPLAY" 2>/dev/null
-            #tmux setenv -g DISPLAY "$DISPLAY" 2>/dev/null
+            # Update the tmux server's environment variables.
+            # This ensures that if you restart Sway/Wayland, new panes in OLD sessions
+            # still have the correct socket paths for the clipboard and windows.
+            REFS_VARS=(
+              # Core Wayland/Sway
+              WAYLAND_DISPLAY SWAYSOCK DISPLAY
+              XDG_RUNTIME_DIR DBUS_SESSION_BUS_ADDRESS
 
-            # Attach to a session named 'main', or create it if it doesn't exist.
-            # 'exec' replaces the bash process so closing tmux closes the terminal.
-            _TMUX_SESSION="$( tmux ls 2>/dev/null | grep -vm1 attached | cut -d: -f1 )"
+              # Identity & Portals
+              XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_ID XDG_SESSION_TYPE
+
+              # Toolkits & NixOS Specifics
+              GDK_BACKEND QT_QPA_PLATFORM SDL_VIDEODRIVER MOZ_ENABLE_WAYLAND
+              NIXOS_OZONE_WL ELECTRON_OZONE_PLATFORM_HINT
+
+              # Auth
+              SSH_AUTH_SOCK
+            )
+            for var in "''${REFS_VARS[@]}"; do
+              if [[ -n "''${!var}" ]]; then
+                tmux setenv -g "$var" "''${!var}" 2>/dev/null
+              fi
+            done
+
+            # We look for the first session  that is not attached (session_attached == 0).
+            _TMUX_SESSION=$(tmux list-sessions -F "#{session_name}:#{session_attached}" 2>/dev/null | awk -F: '$2=="0" {print $1; exit}')
+
             if [[ -z "$_TMUX_SESSION" ]] ;then
-              exec tmux -2 -u new-session
-            # if detached session available attach to it
+              exec tmux -u new-session
             else
-              exec tmux -2 -u attach-session -t "$_TMUX_SESSION"
+              # ADD: Refreshes the local client environment upon attachment.
+              exec tmux -u attach-session -t "$_TMUX_SESSION"
             fi
           fi
         fi
@@ -184,13 +207,30 @@ in
       nix-direnv.enable = true;
     };
 
+    programs.eza = lib.mkIf cfg.enableEza {
+      enable = true;
+      colors = "auto";
+      git = true;
+      enableBashIntegration = true;
+      extraOptions = [
+        "--group-directories-first"
+        "--header"
+      ];
+    };
+    programs.dircolors = lib.mkIf cfg.enableDircolors {
+      enable = true;
+      enableBashIntegration = true;
+    };
+    programs.bat = lib.mkIf cfg.enableBat {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch batpipe prettybat ];
+    };
+
     # Install required packages
     home.packages = [
       pkgs.xdg-utils  # used for 'open' alias (xdg-open)
     ]
     ++ lib.optionals cfg.enableCompletion [ pkgs.bash-completion pkgs.nix-bash-completions ]
-    ++ lib.optionals cfg.aliases.use_bat [ pkgs.bat ]
-    ++ lib.optionals cfg.aliases.use_eza [ pkgs.eza ]
     ++ lib.optionals cfg.autoAttachTmux [ pkgs.tmux ];
   };
 }
