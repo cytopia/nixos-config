78ea1ad7c05f5f4d6090369dc8bee201243b62fa
cytopia <cytopia@everythingcli.org>
2026-04-19 17:50:33 +0200

    firewall

--- hosts/common/vars-opensnitch-rules/binutils-sys.nix
+++ hosts/common/vars-opensnitch-rules/binutils-sys.nix
@@ -8,12 +8,17 @@ let
   gitRemoteHttp = {
     name = "git-remote-http";
     regPath = "^/nix/store/[^/]+-git-.*/libexec/git-core/git-remote-http$";
-    uid = uid;
+    uid = "${toString uid}";
+  };
+  ssh = {
+    name = "ssh";
+    regPath = "^/nix/store/[^/]+-openssh-.*/bin/ssh$";
+    uid = "${toString uid}";
   };
 
 
   ###
-  ### nix flake update
+  ### git
   ###
   mkGitRemoteHttps = name: regexPath: uid: {
     "${rulePrefix}-${name}-${uid}-fetch-https" = {
@@ -53,11 +58,53 @@ let
     };
   };
 
+  ###
+  ### Git over SSH
+  ###
+  mkGitSsh = name: regexPath: uid: {
+    "${rulePrefix}-${name}-${uid}-ssh-git" = {
+      action = "allow";
+      precedence = true;
+      created = "2026-01-01T00:00:00+00:00";
+      updated = "2026-01-01T00:00:00+00:00";
+      name = "xxx      ${name} [${uid}] -> *:22 [TCP]  (SSH)";
+      enabled = true;
+      duration = "always";
+      operator = {
+        type = "list";
+        operand = "list";
+        list = [
+          {
+            type = "regexp";
+            operand = "process.path";
+            data = regexPath;
+          }
+          {
+            type = "simple";
+            operand = "user.id";
+            data = uid;
+          }
+          {
+            type = "simple";
+            operand = "protocol";
+            data = "TCP";
+          }
+          {
+            type = "simple";
+            operand = "dest.port";
+            data = "22";
+          }
+        ];
+      };
+    };
+  };
+
 
 in
 {
   rules = { }
-    // mkGitRemoteHttps gitRemoteHttp.name gitRemoteHttp.regPath "${toString gitRemoteHttp.uid}"
+    // mkGitRemoteHttps gitRemoteHttp.name gitRemoteHttp.regPath gitRemoteHttp.uid
+    // mkGitSsh ssh.name ssh.regPath ssh.uid
   ;
 }
 
